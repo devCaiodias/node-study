a80e343194a79995dc2ab66e85affb52384d6f96
devCaiodias <caiodiasmartins12@gmail.com>
2025-06-24 16:34:18 -0300

    Creating the books get route

--- src/app.js
+++ src/app.js
@@ -3,8 +3,23 @@ import { StatusCodes } from 'http-status-codes'
 
 const app = express()
 
+const books = [
+    {
+        id: 1,
+        title: 'Codigo Limpo',
+    },
+    {
+        id: 2,
+        title: 'Principios SOLID',
+    }
+]
+
 app.get('/', (_req, res) => {
     res.status(StatusCodes.OK).send('Apendendo node com express')
+})
+
+app.get('/books', (_req, res) => {
+    res.status(StatusCodes.OK).json(books)
 })
 
 export default app
