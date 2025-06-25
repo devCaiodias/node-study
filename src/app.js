562509c74b30b21311a64264c4f5e7f0914d7ffd
devCaiodias <caiodiasmartins12@gmail.com>
2025-06-25 14:42:12 -0300

    Refactor book routes: add GET by ID, PUT, and DELETE methods

--- src/app.js
+++ src/app.js
@@ -15,12 +15,15 @@ const books = [
     }
 ]
 
-app.get('/', (_req, res) => {
-    res.status(StatusCodes.OK).send('Apendendo node com express')
-})
-
 app.get('/books', (_req, res) => {
     res.status(StatusCodes.OK).json(books)
+})
+
+app.get('/books/:id', (req, res) => {
+    const booksId = req.params.id
+    const book = books.findIndex(book => book.id === Number(booksId))
+    
+    res.status(StatusCodes.OK).json(books[book])
 })
 
 app.post('/books', (req, res) => {
@@ -29,4 +32,22 @@ app.post('/books', (req, res) => {
     res.status(StatusCodes.CREATED).json(req.body)
 })
 
-export default app
+app.put('/books/:id', (req, res) => {
+    const booksId = req.params.id
+    const bookIndex = books.findIndex(book => book.id === Number(booksId))
+    books[bookIndex].title = req.body.title
+
+    res.status(StatusCodes.OK).json(books)
+    
+})
+
+app.delete('/books/:id', (req, res) => {
+    const booksId = req.params.id
+    const bookIndex = books.findIndex(book => book.id === Number(booksId))
+
+    books.splice(bookIndex, 1)
+
+    res.status(StatusCodes.NO_CONTENT).json({mesage: 'Livro removido com sucesso!'})
+})
+
+export default app;
